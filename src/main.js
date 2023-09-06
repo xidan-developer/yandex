0ad6d4d232a89f835b45b9a50eb8e3a6f57ee23f
xidan-developer <mezencev.daniil@icloud.com>
2023-09-06 15:11:30 +0700

    второй

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import StoryblokVue from 'storyblok-vue'
-import VueAnalytics from 'vue-analytics'
 
 
 Vue.config.productionTip = false
-const isProd = process.env.NODE_ENV === "production"
 
 new Vue({
   router,
@@ -15,11 +13,4 @@ new Vue({
 }).$mount('#app')
 
 Vue.use(StoryblokVue)
-Vue.use(VueAnalytics, {
-  id: 'UA-139190314-1',
-  router,
-  debug: {
-    enabled: !isProd,
-    sendHitTask: isProd
-  }
-})
+
